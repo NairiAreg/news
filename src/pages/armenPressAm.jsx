112456dc2e56c38bf3a41f768c16e15ae9f59603
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 <hnairiareg@gmail.com>
2023-06-05 04:56:25 +0400

    Hot fixes, news.am and armnews.am

--- src/pages/armenPressAm.jsx
+++ src/pages/armenPressAm.jsx
@@ -1,45 +1,73 @@
-import { Container } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { getNewsByDateNewsAm, getNewsByDateArmenPress } from "@/services";
-import { useDate } from "@/hooks";
-import Head from "next/head";
+import {
+  // getNewsByDateNewsAm,
+  // getNewsByDateArmenPress,
+  getNewsByDateArmenPress,
+} from "@/services";
+import { useDate, useLocalStorage } from "@/hooks";
 import { useTranslation } from "@/contexts/TranslationContext";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Navbar, CardList } from "@/components";
+import { CardList, MainLayout } from "@/components";
 
-export default function ArmenPressAm() {
-  const { t, language } = useTranslation();
+export default function Home() {
+  const { language } = useTranslation();
   const { getStructuredDate } = useDate();
   const [parsedHTML, setParsedHTML] = useState();
+  const [searchData, setSearchData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const [startDate, setStartDate] = useState(new Date());
+  const { put, get } = useLocalStorage();
+
+  useEffect(() => {
+    if (parsedHTML) {
+      setIsLoading(false);
+    }
+    setSearchData(parsedHTML);
+
+    if (!parsedHTML || !startDate) {
+      return;
+    }
+    const cacheDate = new Date(startDate).toLocaleDateString();
+    if (!get(cacheDate) && parsedHTML) {
+      put(cacheDate, JSON.stringify(parsedHTML?.slice(10))); //TODO change 10 to LIMIT
+    }
+  }, [parsedHTML, startDate]);
+
+  console.log("🟢", searchData);
+
   useEffect(() => {
     if (!language) {
       return;
     }
-    getNewsByDateArmenPress({ ...getStructuredDate(startDate), language }).then(
-      (data) => setParsedHTML(data)
-    );
 
-    getNewsByDateNewsAm({ ...getStructuredDate(startDate), language }).then(
-      (data) => console.log("🔴 news.am", data)
-    );
+    getNewsByDateArmenPress({
+      ...getStructuredDate(startDate),
+      language,
+    }).then((data) => {
+      setParsedHTML(data);
+      setIsLoading(false);
+    });
+
+    // getAllNewsByDate({
+    //   ...getStructuredDate(startDate),
+    //   language,
+    // }).then((data) => {
+    //   setParsedHTML(data);
+    //   setIsLoading(false);
+    // });
   }, [language, startDate]);
+
+  console.log("😅", parsedHTML);
+
   return (
-    <>
-      <Navbar data={parsedHTML} setParsedHTML={setParsedHTML} />
-      <Head>
-        <title>{t("news")}</title>
-      </Head>
-      <main>
-        <Container maxW="container.xl">
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
-          <CardList data={parsedHTML} />
-        </Container>
-      </main>
-    </>
+    <MainLayout
+      data={parsedHTML}
+      setData={setSearchData}
+      startDate={startDate}
+      setStartDate={setStartDate}
+      setIsLoading={setIsLoading}
+    >
+      <CardList data={searchData} isLoading={isLoading} date={startDate} />
+    </MainLayout>
   );
 }
--- src/pages/newsAm.jsx
+++ src/pages/newsAm.jsx
@@ -1,47 +1,78 @@
-import { Container } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { getNewsByDateNewsAm, getNewsByDateArmenPress } from "@/services";
-import { useDate } from "@/hooks";
-import Head from "next/head";
+import {
+  // getNewsByDateNewsAm,
+  // getNewsByDateArmenPress,
+  getNewsByDateNewsAm,
+} from "@/services";
+import { useDate, useLocalStorage } from "@/hooks";
 import { useTranslation } from "@/contexts/TranslationContext";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { Navbar, CardList } from "@/components";
+import { CardList, MainLayout } from "@/components";
 
-export default function NewsAm() {
-  const { t, language } = useTranslation();
+export default function Home() {
+  const { language } = useTranslation();
   const { getStructuredDate } = useDate();
   const [parsedHTML, setParsedHTML] = useState();
+  const [searchData, setSearchData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const [startDate, setStartDate] = useState(new Date());
+  const { put, get } = useLocalStorage();
+
+  useEffect(() => {
+    if (parsedHTML) {
+      setIsLoading(false);
+    }
+    setSearchData(parsedHTML);
+
+    if (!parsedHTML || !startDate) {
+      return;
+    }
+    const cacheDate = new Date(startDate).toLocaleDateString();
+    if (!get(cacheDate) && parsedHTML) {
+      put(cacheDate, JSON.stringify(parsedHTML?.slice(10))); //TODO change 10 to LIMIT
+    }
+  }, [parsedHTML, startDate]);
+
+  console.log("🟢", searchData);
+
   useEffect(() => {
     if (!language) {
       return;
     }
-    getNewsByDateArmenPress({
-      ...getStructuredDate(startDate),
-      language,
-    }).then((data) => console.log("🔵 armenpress.am", data));
-
     getNewsByDateNewsAm({
       ...getStructuredDate(startDate),
       language,
-    }).then((data) => setParsedHTML(data));
+    }).then((data) => {
+      setParsedHTML(data);
+      setIsLoading(false);
+    });
+
+    // getNewsByDateArmenPress({
+    //   ...getStructuredDate(startDate),
+    //   language,
+    // });
+    // // .then((data) => console.log("🔵 armenpress.am", data));
+
+    // getAllNewsByDate({
+    //   ...getStructuredDate(startDate),
+    //   language,
+    // }).then((data) => {
+    //   setParsedHTML(data);
+    //   setIsLoading(false);
+    // });
   }, [language, startDate]);
+
+  console.log("😅", parsedHTML);
+
   return (
-    <>
-      <Navbar data={parsedHTML} setParsedHTML={setParsedHTML} />
-      <Head>
-        <title>{t("news")}</title>
-      </Head>
-      <main>
-        <Container maxW="container.xl">
-          <DatePicker
-            selected={startDate}
-            onChange={(date) => setStartDate(date)}
-          />
-          <CardList data={parsedHTML} />
-        </Container>
-      </main>
-    </>
+    <MainLayout
+      data={parsedHTML}
+      setData={setSearchData}
+      startDate={startDate}
+      setStartDate={setStartDate}
+      setIsLoading={setIsLoading}
+    >
+      <CardList data={searchData} isLoading={isLoading} date={startDate} />
+    </MainLayout>
   );
 }
